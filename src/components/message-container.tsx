436d37dcb2ce47e6b8ae70df9460fd8d2c1a55c5
anbuzin <contactbuzin@gmail.com>
2025-05-16 14:09:59 +0700

    Add typography to make markdown prettier

--- src/components/message-container.tsx
+++ src/components/message-container.tsx
@@ -1,28 +1,30 @@
-import { cn } from "@/lib/utils"
+import Markdown from "react-markdown";
+import { cn } from "@/lib/utils";
 
 export type Message = {
-    id: string;
-    llm_role: string;
-    body: string;
-    created_at: string;
-    chatId: string;
-    evicted?: boolean;
-    thinking?: string;
-}
+  id: string;
+  llm_role: string;
+  body: string;
+  created_at: string;
+  chatId: string;
+  evicted?: boolean;
+  thinking?: string;
+};
 
 export function MessageContainer({ message }: { message: Message }) {
-    return (
-        <div key={message.id} className={cn("flex flex-col w-full")}>
-            <div
-                className={cn(
-                    "rounded-lg p-4 w-fit max-w-[80%]",
-                    message.llm_role === "user"
-                        ? "bg-primary text-primary-foreground self-end"
-                        : "bg-secondary text-secondary-foreground self-start",
-                )}
-            >
-                <p>{message.body}</p>
-            </div>
-        </div>
-    );
+  const isUser = message.llm_role === "user";
+  return (
+    <div key={message.id} className={cn("flex flex-col w-full")}>
+      <div
+        className={cn(
+          "rounded-lg p-4 w-fit max-w-[80%]",
+          isUser
+            ? "prose bg-primary text-primary-foreground self-end"
+            : "prose bg-secondary text-secondary-foreground self-start"
+        )}
+      >
+        <Markdown>{message.body}</Markdown>
+      </div>
+    </div>
+  );
 }
