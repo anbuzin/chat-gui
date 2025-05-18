14d69667b1408496dac08e8e2d1fdd6671c3bf56
anbuzin <contactbuzin@gmail.com>
2025-05-18 23:33:10 +0700

    Populate the sidebar with stuff

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,9 +1,38 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { mockChats } from "@/app/mock-chats";
+
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Today</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {mockChats.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={`/chat/${item.id}`}>
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
-  )
+  );
 }
