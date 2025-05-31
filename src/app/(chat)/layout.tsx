68137d7a25f80c09a470bd388a855a85f1dc8ede
anbuzin <contactbuzin@gmail.com>
2025-05-31 10:44:37 +0700

    Remove superfluous token handling, it's supposed to be in cookies

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -2,7 +2,6 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
-import { AuthProvider } from "@/components/auth-provider";
 
 export default async function ChatLayout({
   children,
@@ -21,14 +20,12 @@ export default async function ChatLayout({
   }
 
   return (
-    <AuthProvider authToken={session.authToken}>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarTrigger className="fixed top-0 left-0 z-50 mt-2 ml-2" />
-        <main className="w-full h-screen flex flex-col">
-          <div className="flex-1 flex flex-col overflow-y-auto">{children}</div>
-        </main>
-      </SidebarProvider>
-    </AuthProvider>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarTrigger className="fixed top-0 left-0 z-50 mt-2 ml-2" />
+      <main className="w-full h-screen flex flex-col">
+        <div className="flex-1 flex flex-col overflow-y-auto">{children}</div>
+      </main>
+    </SidebarProvider>
   );
 }
